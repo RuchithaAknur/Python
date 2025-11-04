2ebadef8adda0069ef6ca4fb542df79585ea8172
RuchithaAknur <akunrruchitha@gmail.com>
2025-11-04 23:46:52 +0530

    Create math_modules.py

---
+++ math_modules.py
@@ -0,0 +1,13 @@
+import math
+print(math.sqrt(25))
+print(math.factorial(5))
+print(math.ceil(4.3))
+print(math.radians(180))
+print(math.pow(2,5))
+print(math.log(100))
+r=7
+area=math.pi*math.pow(r,2)
+print(area)
+
+
+
