8ddd858e1caf8ee4030dd3ecefa9135aecbd5dcf
RuchithaAknur <akunrruchitha@gmail.com>
2025-11-05 23:22:42 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,43 @@
+s={1,2,3}
+# s.add(4)
+# print(s)
+
+# s={1,2,3}
+# s.discard(2)
+# print(s)
+
+# s={1,2}
+# t={2,3}
+# x=s|t
+# print(x)
+
+# s={1,2}
+# t={2,3}
+# x=s&t
+# print(x)
+
+
+# s={1,2}
+# t={2,3}
+# x=s-t
+# print(x)
+
+# s={1,2}
+# t={1,2,3}
+# print(s.issubset(t))
+
+# s={1,2,3}
+# x=len(s)
+# print(x)
+
+# s={1,2,3}
+# s.clear()
+# print(s)
+
+# s={1,2,3}
+# t={2,3,4}
+# print(s^t)
+
+# l=[1,2,2,3]
+# x=set(l)
+# print(x)
